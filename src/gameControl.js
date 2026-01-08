87ab006683545091893d2b708c46fb1bb76bed0d
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2026-01-08 17:15:15 -0500

    implement control logic

---
+++ src/gameControl.js
@@ -0,0 +1,41 @@
+import { Player, ComputerPlayer } from "./player";
+import { GameBoard } from "./gameboard";
+
+export class GameController {
+    constructor(player1, player2) {
+        this.currentPlayer = player1;
+        this.waitingPlayer = player2;
+        this.gameOver = false;
+        this.winner = null;
+    }
+
+    playTurn(x = null, y = null) {
+        if (this.gameOver) return null;
+
+        const result = this.currentPlayer.takeTurn(
+            this.waitingPlayer.gameBoard,
+            x,
+            y
+        );
+
+        if (this.waitingPlayer.gameBoard.isGameOver()) {
+            this.gameOver = true;
+            this.winner = this.currentPlayer;
+            return { result, winner: this.winner };
+        }
+
+        this.switchTurns();
+        return { result };
+    }
+
+    switchTurns() {
+        [this.currentPlayer, this.waitingPlayer] =
+            [this.waitingPlayer, this.currentPlayer];
+    }
+
+    startGame() {
+        this.currentPlayer.gameBoard.placeBoard();
+        this.waitingPlayer.gameBoard.placeBoard();
+    }
+
+}
